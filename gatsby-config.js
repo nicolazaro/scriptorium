35ab9f1cd29a9a9b8b5a85dc9ee8c1f427efa646
daniphr1 <daniphr1@umd.edu>
2022-10-12 13:57:26 -0400

    descripcion

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
   pathPrefix: basePath,
   siteMetadata: {
     title: `Digital Publishing with Minimal Computing Project`,
-    description: `A template`,
-    author: `Course teachers`
+    description: `SCRIPTORIVM`,
+    author: `Graciela, Nicolás y Daniela`
   },
   plugins: [
     `gatsby-plugin-material-ui`,
